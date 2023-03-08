4d4dfb923fec4ee75c935ea008b58bcbe81dc356
Seiji Tateishi <seijitateishi@hotmail.com>
2023-03-08 12:04:04 -0300

    Delete Cadastro directory

--- Cadastro/src/main/java/com/seiji/cadastro/repository/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seiji.cadastro.repository;
-
-import com.seiji.cadastro.object.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Long> {
-}
