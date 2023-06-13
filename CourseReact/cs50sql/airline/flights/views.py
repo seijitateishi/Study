8cb8b28759ae3ff32ef6970bd11c753e75305da8
seiji <seijitateishi@hotmail.com>
2023-06-13 02:53:26 -0300

    everything working till django admin section

--- CourseReact/cs50sql/airline/flights/views.py
+++ CourseReact/cs50sql/airline/flights/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from .models import Flight
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
